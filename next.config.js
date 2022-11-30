2eaaadbd0153621bb47c775c85a3a45d5662f515
mj MackBook <leemj0948@gmail.com>
2022-11-30 14:22:31 +0900

    add:redirect 설정

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects(){
+    return [
+      {
+        source:"/request/:path(\\d[2])",
+      destination:"/request/:path(\\d[1])",
+      permanent:true,
+    }
+    ]
+  }
 }
 
 module.exports = nextConfig
