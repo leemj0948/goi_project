05b48f96092fd1263e05380d1fc92c2c095c9204
mj MackBook <leemj0948@gmail.com>
2022-11-30 17:59:20 +0900

    add:step2 디자인 완성

--- pages/guidebook.tsx
+++ pages/guidebook.tsx
@@ -5,16 +5,63 @@ export default function guidebook(){
             <Title>가이드북 검색</Title>
             <InputArea>
             <input placeholder='텍스트를 입력해주세요.'/>
+            <div><p>29</p>건의 검색 결과가 있습니다.</div>
             </InputArea>
+            <SearchList>
+                <ul><p>•</p> 장례식 부조금 봉투 쓰는 법</ul>
+                <SubList>(장례 상식 / 2022-10-31)</SubList>
+            </SearchList>
         </Wapper>
     )
 }
 const Wapper = styled.section`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: space-between;
+  padding: 2.5rem 1rem 0;
   height: 100%;
+  min-height: 575px;
   width: auto;
 `;
-const Title = styled.h1``;
-const InputArea = styled.div``;
+const Title = styled.h1`
+margin-bottom: 1rem;
+font-size: 1.5rem;
+font-weight: bold;
+`;
+const InputArea = styled.div`
+height: 50px;
+input{
+    padding: 10px;
+    width: 90%;
+    height: 90%;
+    background-color: #d3d3d3;
+
+    border: none;
+    border-radius: 5px;
+
+}
+div{
+    display: flex;
+    font-weight: bold;
+    margin: 0.5rem;
+    p{
+        color: green;
+    }
+}
+`;
+const SearchList =styled.div`
+margin-top: 50px;
+ul {
+   display:flex;
+   font-size: 1.5rem;
+   font-weight: bold;
+   p{
+    margin-right:5px;
+   }
+}
+
+`;
+const SubList =styled.p`
+font-size:1.1rem;
+padding: 10px 0 0 20px;
+color: grey;`
